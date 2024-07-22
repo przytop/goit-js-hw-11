a56d65a6d31e74ccd0f02d58acc3c94067c280e0
przytop <102540215+przytop@users.noreply.github.com>
2024-07-22 21:27:03 +0000

    Deploying to gh-pages from @ przytop/goit-js-hw-11@943f73349df949305d1d844213987f4f3ce1d8dd 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,24 +1,24 @@
-import"./assets/styles-f3635d75.js";import{S as p,i as y}from"./assets/vendor-8c59ed88.js";const d="45020443-554fa7ec416e1f918b290a17c",f="https://pixabay.com/api/";function S(e){const t=new URLSearchParams({key:d,q:e,image_type:"photo",orientation:"horizontal",safesearch:!0});return fetch(`${f}?${t}`).then(a=>{if(!a.ok)throw new Error(a.status);return a.json()}).catch(a=>console.log(a))}const s=document.querySelector(".form"),r=document.querySelector(".loader"),i=document.querySelector(".gallery"),$=new p(".gallery a",{captionsData:"alt",captionDelay:250});function b(){r.classList.remove("disabled")}function l(){r.classList.add("disabled")}s.addEventListener("submit",e=>{e.preventDefault();const t=e.target.search.value.trim();i.innerHTML="",b(),S(t).then(a=>{if(a.hits.length===0){y.error({message:"Sorry, there are no images matching your search query. Please try again!",position:"topRight"}),l();return}l(),i.innerHTML=a.hits.map(({webformatURL:o,largeImageURL:n,tags:c,likes:m,views:h,comments:g,downloads:u})=>`
+import"./assets/styles-f3635d75.js";import{S as y,i}from"./assets/vendor-8c59ed88.js";const f="45020443-554fa7ec416e1f918b290a17c",d="https://pixabay.com/api/";function S(a){const t=new URLSearchParams({key:f,q:a,image_type:"photo",orientation:"horizontal",safesearch:!0});return fetch(`${d}?${t}`).then(e=>{if(!e.ok)throw new Error(e.status);return e.json()}).catch(e=>console.log(e))}const s=document.querySelector(".form"),o=document.querySelector(".loader"),l=document.querySelector(".gallery"),$=new y(".gallery a",{captionsData:"alt",captionDelay:250});function b(){o.classList.remove("disabled")}function r(){o.classList.add("disabled")}s.addEventListener("submit",a=>{a.preventDefault();const t=a.target.search.value.trim();if(t===""){i.warning({message:"Complete the field correctly",position:"topRight"});return}l.innerHTML="",b(),S(t).then(e=>{if(e.hits.length===0){i.error({message:"Sorry, there are no images matching your search query. Please try again!",position:"topRight"}),r();return}r(),l.innerHTML=e.hits.map(({webformatURL:n,largeImageURL:c,tags:m,likes:h,views:g,comments:u,downloads:p})=>`
           <li class="gallery-item">
-            <a class="gallery-link" href="${n}">
-              <img class="gallery-image" src="${o}" alt="${c}">
+            <a class="gallery-link" href="${c}">
+              <img class="gallery-image" src="${n}" alt="${m}">
             </a>
             <ul class="image-info">
               <li>
                 <h3 class="image-title">Likes</h3>
-                <p class="image-stats">${m}</p>
-              </li>
-              <li>
-                <h3 class="image-title">Views</h3>
                 <p class="image-stats">${h}</p>
               </li>
               <li>
-                <h3 class="image-title">Comments</h3>
+                <h3 class="image-title">Views</h3>
                 <p class="image-stats">${g}</p>
               </li>
               <li>
-                <h3 class="image-title">Downloads</h3>
+                <h3 class="image-title">Comments</h3>
                 <p class="image-stats">${u}</p>
+              </li>
+              <li>
+                <h3 class="image-title">Downloads</h3>
+                <p class="image-stats">${p}</p>
               </li>
             </ul>
           </li>`).join(""),$.refresh()}),s.reset()});
