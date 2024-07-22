d0d7c16d9787d02984f2b3f2e0a2495cd81a24d7
przytop <102540215+przytop@users.noreply.github.com>
2024-07-22 20:51:16 +0000

    Deploying to gh-pages from @ przytop/goit-js-hw-11@2976520a2fafae31ebdd8a5554269a4db601bc5b 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,2 +1,25 @@
-(function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))s(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const o of t.addedNodes)o.tagName==="LINK"&&o.rel==="modulepreload"&&s(o)}).observe(document,{childList:!0,subtree:!0});function i(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function s(e){if(e.ep)return;e.ep=!0;const t=i(e);fetch(e.href,t)}})();
+import"./assets/styles-f3635d75.js";import{S as p,i as y}from"./assets/vendor-8c59ed88.js";const d="45020443-554fa7ec416e1f918b290a17c",f="https://pixabay.com/api/";function S(e){const t=new URLSearchParams({key:d,q:e,image_type:"photo",orientation:"horizontal",safesearch:!0});return fetch(`${f}?${t}`).then(a=>{if(!a.ok)throw new Error(a.status);return a.json()}).catch(a=>console.log(a))}const s=document.querySelector(".form"),r=document.querySelector(".loader"),i=document.querySelector(".gallery"),$=new p(".gallery a",{captionsData:"alt",captionDelay:250});function b(){r.classList.remove("disabled")}function l(){r.classList.add("disabled")}s.addEventListener("submit",e=>{e.preventDefault();const t=e.target.search.value.trim();i.innerHTML="",b(),S(t).then(a=>{if(a.hits.length===0){y.error({message:"Sorry, there are no images matching your search query. Please try again!",position:"topRight"}),l();return}l(),i.innerHTML=a.hits.map(({webformatURL:o,largeImageURL:n,tags:c,likes:m,views:h,comments:g,downloads:u})=>`
+          <li class="gallery-item">
+            <a class="gallery-link" href="${n}">
+              <img class="gallery-image" src="${o}" alt="${c}">
+            </a>
+            <ul class="image-info">
+              <li>
+                <h3 class="image-title">Likes</h3>
+                <p class="image-stats">${m}</p>
+              </li>
+              <li>
+                <h3 class="image-title">Views</h3>
+                <p class="image-stats">${h}</p>
+              </li>
+              <li>
+                <h3 class="image-title">Comments</h3>
+                <p class="image-stats">${g}</p>
+              </li>
+              <li>
+                <h3 class="image-title">Downloads</h3>
+                <p class="image-stats">${u}</p>
+              </li>
+            </ul>
+          </li>`).join(""),$.refresh()}),s.reset()});
 //# sourceMappingURL=commonHelpers.js.map
